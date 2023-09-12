071bb000e2e4360f8f4e16416a9db74853f6735e
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-09-12 10:55:55 +0500

    amaliyot

--- 1-topshiriq/username.py
+++ 1-topshiriq/username.py
@@ -1,9 +1,11 @@
 import random
 ism  = input('Username kiriting ')
+try:
+    def user_name(x):
+        son = random.randint(0,9)
+        teskari = x[::-1]
+        print(f'{teskari}{son}')
 
-def user_name(x):
-    son = random.randint(0,9)
-    teskari = x[::-1]
-    print(f'{teskari}{son}')
-
-user_name(ism)
+    user_name(ism)
+except:
+    print('Ismni tog`ri kiritmadingiz ')
